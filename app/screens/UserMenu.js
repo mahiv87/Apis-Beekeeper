4bb8882f5d6d5f1f799553c367584e21e8458df6
Marcus mbp <maherreraiv1987@gmail.com>
2022-12-01 20:57:21 -0700

    added image picker to user menu

--- app/screens/UserMenu.js
+++ app/screens/UserMenu.js
@@ -1,16 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 import Screen from '../components/Screen';
 import Button from '../components/Button';
 
 export default function UserMenu() {
+	const pickImageAsync = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			allowsEditing: true,
+			quality: 1
+		});
+
+		if (!result.canceled) {
+			console.log(result);
+		} else {
+			alert('You did not select any image.');
+		}
+	};
+
 	return (
 		<Screen>
 			<View style={[styles.buttonsContainer]}>
 				<MaterialCommunityIcons name="account" size={48} color="black" />
-				<Button title="Upload photo" color="secondary" />
+				<Button title="Upload photo" color="secondary" onPress={pickImageAsync} />
 			</View>
 			<View></View>
 		</Screen>
