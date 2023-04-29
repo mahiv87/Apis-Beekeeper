55cd29d5df075936413209ef19f82844048e09ce
mahiv87 <maherreraiv1987@gmail.com>
2023-04-29 09:49:28 -0600

    file name refactors and cleanup

--- app/components/TextInput.js
+++ app/components/TextInput.js
@@ -7,8 +7,19 @@ import defaultStyles from '../config/styles';
 function AppTextInput({ icon, width = '100%', ...otherProps }) {
 	return (
 		<View style={[styles.container, { width }]}>
-			{icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
-			<TextInput placeholderTextColor={defaultStyles.colors.medium} style={defaultStyles.text} {...otherProps} />
+			{icon && (
+				<MaterialCommunityIcons
+					name={icon}
+					size={20}
+					color={defaultStyles.colors.medium}
+					style={styles.icon}
+				/>
+			)}
+			<TextInput
+				placeholderTextColor={defaultStyles.colors.medium}
+				style={defaultStyles.text}
+				{...otherProps}
+			/>
 		</View>
 	);
 }
