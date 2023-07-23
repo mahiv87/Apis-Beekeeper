9ffa8b7cdc7f388633f36a7e803cce3651cba5cc
mahiv87 <maherreraiv1987@gmail.com>
2023-07-23 13:03:26 -0600

    added background to welcome screen

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React, { useState, useEffect, useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ProfileScreen from './app/screens/ProfileScreen';
@@ -8,12 +10,48 @@ import UserMenu from './app/screens/UserMenu';
 import LoginScreen from './app/screens/LoginScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
 import UploadScreen from './app/screens/UploadScreen';
+import AuthContext from './app/auth/context';
+import authStorage from './app/auth/storage';
 import { navigationRef } from './app/navigation/rootNavigation';
 import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
+	const [user, setUser] = useState();
+	const [appIsReady, setAppIsReady] = useState(false);
+
+	const restoreUser = async () => {
+		const user = await authStorage.getUser();
+		if (user) setUser(user);
+	};
+
+	useEffect(() => {
+		async function prepare() {
+			try {
+				await SplashScreen.preventAutoHideAsync();
+				const result = await restoreUser();
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				setAppIsReady(true);
+			}
+		}
+
+		prepare();
+	}, []);
+
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			const result = await SplashScreen.hideAsync();
+			// console.log(result, 'hide?');
+		}
+	}, [appIsReady]);
+
+	if (!appIsReady) {
+		return null;
+	}
+
 	return (
-		<NavigationContainer ref={navigationRef}>
+		<NavigationContainer ref={navigationRef} onReady={onLayoutRootView}>
 			<AuthNavigator />
 		</NavigationContainer>
 		// <WelcomeScreen />
